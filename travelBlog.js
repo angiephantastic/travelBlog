474f931f7cd0a98ae31a3d28ebeff205abc5c4ab
angiephantastic <nguyenphan1395@gmail.com>
2020-10-14 20:20:38 +0200

    Comments remvd. weatherInfo added to eachPlace but returning undefined values.

--- travelBlog.js
+++ travelBlog.js
@@ -5,52 +5,42 @@ const cityInput = document.getElementById("cityInput")
 const countryInput = document.getElementById("countryInput")
 const dateInput = document.getElementById("dateInput")
 
-//initial view on load 
 const getInfo = () => {
-    const places = localStorage.getItem("places")
-
+    let places = localStorage.getItem("places")
+    
     if(!places) {
         return []
     }
+    places = JSON.parse(places)
 
-    return JSON.parse(places)
-    //data received from local returns a string
-    //JSON.parse translate it into an object
-}
-
-
-const getWeatherInfo = () => {
-    const key = "d6f595f46674408259ff7b3919758466"
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&appid=${key}`)
-    .then((response) => response.json())
-    .then((json) => {
-        const info = json
-        const condition = info.weather[0].icon
-
-        let someText = `
-        <p>Current weather: ${info.main.temp}°C</p>
-        <img src="http://openweathermap.org/img/wn/${condition}@2x.png">
-        `
-        return someText
+    places.forEach((place) => {
+        place.weatherInfo = getWeatherInfo(place.city)
     })
+    return places
 }
 
-console.log(getWeatherInfo())
+function getWeatherInfo(cityName) {
+    const key = "d6f595f46674408259ff7b3919758466"
+    // const cityName = getInfo(place.city)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric`)
+        .then((response) => response.json())
+        .then((json) => {
+            const info = json
+            const weather = info.main.temp
+        // console.log(cityName)
+        // console.log(weather)
+        return weather 
+        })
+}
 
-//takes eachPlace and adds it to a list of places
 const savePlaces = (eachPlace) => {
     const places = getInfo()
     places.push(eachPlace)
 
     const stringifiedPlaces = JSON.stringify(places)
-    //const places calls getInfo() and returns an object
-    //then JSON.strigify translate it back into strings
-    //then saves to local storage below
-
     localStorage.setItem("places", stringifiedPlaces)
 }
 
-//create a newPlace with input info
 const newPlace = (event) => {
     event.preventDefault()
 
@@ -61,10 +51,9 @@ const newPlace = (event) => {
     const eachPlace = {
         city,
         country,
-        date
+        date,
     }
     
-    //reset input fields
     cityInput.value = ""
     countryInput.value = ""
     dateInput.value = ""
@@ -72,31 +61,23 @@ const newPlace = (event) => {
     addDestination(eachPlace)
 }
 
-//newPlace() is called when the "submit" button is clicked
 addLocationForm.addEventListener("submit", newPlace)
 
-
-//takes eachPlace and calls savePlaces
-//then create a new div with createInfo()
 const addDestination = (eachPlace) => {
     savePlaces(eachPlace)
     createInfo()
 }
 
-//create text output inside div that is created
-//with createInfo()
 const textDescription = (eachPlace) => {
     let someText = `
     <p>City: ${eachPlace.city}</p>
     <p>Country: ${eachPlace.country}</p>
     <p>Travel Date: ${eachPlace.date}</p>
-    <p>Current Weather: </p>
+    <p>Current Weather: ${eachPlace.weatherInfo}</p>
     `
     return someText
 }
 
-//create a div with all contents and a remove button
-//with an event listener that calls removePlace() when click
 const createInfo = () => {
     detailDescription.innerHTML = ""
 
@@ -119,7 +100,6 @@ const createInfo = () => {
     })
 }
 
-//remove button function
 const removePlace = (index) => {
     const places = getInfo()
     places.splice(index, 1)
